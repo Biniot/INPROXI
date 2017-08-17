c2cfc5317c5a12d52464ae2985b81920af6787d5
Erwin Sanquer <erwin.sanquer@epitech.eu>
2017-08-17 23:25:02 +0200

    Gestion utilisateur début

--- inproxy-mobile/src/app/app.module.ts
+++ inproxy-mobile/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { HttpModule } from '@angular/http';
+import { Camera } from '@ionic-native/camera';
 
 import { AuthServiceProvider } from '../providers/auth-service/auth-service'
 import { HttpRequestProvider } from '../providers/http-request/http-request';
@@ -12,10 +13,12 @@ import { RoomsPage } from '../pages/rooms/rooms';
 import { UserPage } from '../pages/user/user';
 import { FriendsPage } from '../pages/friends/friends';
 import { LoginPage } from '../pages/login/login';
+import {EditUserPage} from "../pages/edit-user/edit-user";
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { NativeStorage } from '@ionic-native/native-storage'
+
 
 @NgModule({
   declarations: [
@@ -24,7 +27,8 @@ import { NativeStorage } from '@ionic-native/native-storage'
     UserPage,
     RoomsPage,
     FriendsPage,
-    LoginPage
+    LoginPage,
+    EditUserPage
   ],
   imports: [
     BrowserModule,
@@ -38,13 +42,15 @@ import { NativeStorage } from '@ionic-native/native-storage'
     UserPage,
     RoomsPage,
     FriendsPage,
-    LoginPage
+    LoginPage,
+    EditUserPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
     {provide: ErrorHandler, useClass: IonicErrorHandler},
     AuthServiceProvider,
+    Camera,
     HttpRequestProvider,
     NativeStorage
   ]
--- inproxy-mobile/src/pages/user/user.ts
+++ inproxy-mobile/src/pages/user/user.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {AlertController, IonicPage, NavController, NavParams} from 'ionic-angular';
+import {AuthServiceProvider, User} from "../../providers/auth-service/auth-service";
 
 /**
  * Generated class for the UserPage page.
@@ -13,12 +14,59 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'user.html',
 })
 export class UserPage {
+  currentUser: User;
+  deleteUserSucces = false;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, private auth: AuthServiceProvider, private alertCtrl: AlertController) {
+    // TODO : Check si c'est le user connecter; Si pas le user (le user regarde un profile d'amis).; Si pas le user retirer les btn de la view
+    this.auth.getUserInfo().subscribe(success => {
+        if (success) {
+          this.currentUser = this.auth.getCurrentUser();
+        } else {
+          this.showPopup("Error", "Problem retriving all profile's information.");
+        }
+      },
+      error => {
+        this.showPopup("Error", error);
+      });
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad UserPage');
+  public editUserNav() {
+    this.navCtrl.push('EditUserPage');
   }
 
+  public deleteUser(){
+    // TODO : pop-up de confirmation
+    this.auth.deleteUser().subscribe(success => {
+        if (success) {
+          this.deleteUserSucces = true;
+          // TODO : Clear currentUser ?
+          this.showPopup("Succes", "Succefully delete user.");
+        } else {
+          this.showPopup("Error", "Problem deleting user.");
+        }
+      },
+      error => {
+        this.showPopup("Error", error);
+      });
+  }
+
+  showPopup(title, text) {
+    let alert = this.alertCtrl.create({
+      title: title,
+      subTitle: text,
+      buttons: [
+        {
+          text: 'OK',
+          handler: () => {
+            if (this.deleteUserSucces) {
+              // TODO : verifier retour qu debut de lapp
+              this.navCtrl.popToRoot();
+            }
+          }
+        }
+      ]
+    });
+    alert.present();
+  }
 }
