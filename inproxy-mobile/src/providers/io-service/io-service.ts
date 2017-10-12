5dc571e22b903fe5ea071dfe150b22f073d90825
Erwin Sanquer <erwin.sanquer@epitech.eu>
2017-10-12 14:33:37 +0200

    Update Io
    Update Io

--- inproxy-mobile/src/providers/io-service/io-service.ts
+++ inproxy-mobile/src/providers/io-service/io-service.ts
@@ -1,5 +1,5 @@
 import {Injectable} from '@angular/core';
-//import * as io from 'socket.io-client';
+import * as io from 'socket.io-client';
 import {API_ADDRESS} from "../constants/constants";
 import {isUndefined} from "ionic-angular/util/util";
 
@@ -19,44 +19,40 @@ export class IoServiceProvider {
   // les argument sur le .on pour le premier c'est un nom d'event.
   constructor() {
     // TODO : je sais pas trop si ca suffit pour nos histoire de token
-    // this.socket = io(API_ADDRESS, {
-    //   path: '',
-    //   reconnection: true,
-    //   query : {token: localStorage.getItem('token')},
-    // });
+    this.socket = io(API_ADDRESS, {
+      path: '',
+      reconnection: true,
+      autoConnect: false
+      //query : {token: localStorage.getItem('token')},
+    });
+    this.socket.on('connect', this.sendAuth);
+    this.socket.on('reconnect', this.sendAuth);
+    //this.socket.on('private_message', this.receiveMessage);
   }
 
-  // sendAuth() {
-  //   this.socket.emit('auth', localStorage.getItem('token'));
-  // }
-  //
-  // // function(){}
-  // public setDisconnectCallback(functionDisconnect: any) {
-  //   this.socket.on('disconnect', functionDisconnect);
-  // }
-  //
-  // // function(){}
-  // public setConnectCallback(functionCallBack: any) {
-  //   this.socket.on('connect', functionCallBack);
-  // }
-  //
-  // // (msg) => { console.log("message", msg); }
-  // public setMessageCallback(functionMessage: any) {
-  //   this.socket.on('message', functionMessage);
-  // }
-  //
-  // public sendMessage(msg: string) {
-  //   if(msg !== '' && msg !== null && !isUndefined(msg)){
-  //     this.socket.emit('message', msg);
-  //   }
-  // }
-  //
-  // public connectSocket() {
-  //   this.socket.open();
-  // }
-  //
-  // public disconnectSocket() {
-  //   this.socket.close();
-  // }
+  sendAuth() {
+    this.socket.emit('auth', localStorage.getItem('token'));
+  }
+
+  receiveMessage(data: any) {
+    // TODO : provider sqlite mis en place dune map
+  }
+
+  // function(){}
+  public setDisconnectCallback(functionDisconnect: any) {
+    this.socket.on('disconnect', functionDisconnect);
+  }
+
+  public sendMessage(from: string, to: string, message: string) {
+    this.socket.emit('private_message', {from: from, to: to, message: message});
+  }
+
+  public connectSocket() {
+    this.socket.open();
+  }
+
+  public disconnectSocket() {
+    this.socket.close();
+  }
 
 }
