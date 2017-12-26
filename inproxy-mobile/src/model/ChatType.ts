f4341abe2afffab740a85e05181487267ebfcabc
Sanquer <erwin.sanquer@epitech.eu>
2017-12-26 12:39:39 +0100

    Update ajout/retrait de zone
    Update ajout/retrait de zone

--- inproxy-mobile/src/model/ChatType.ts
+++ inproxy-mobile/src/model/ChatType.ts
@@ -1,6 +1,6 @@
 export let ChatType = {
   STD_CONVERSATION: 1,
-  ROOM_CONVERSATIon: 2,
+  ROOM_CONVERSATION: 2,
   properties: {
     1: {name: "StdConversation", value: 1},
     2: {name: "RoomConversation", value: 2}
--- inproxy-mobile/src/pages/list-chat/list-chat.ts
+++ inproxy-mobile/src/pages/list-chat/list-chat.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
-import {Events, IonicPage, NavController} from 'ionic-angular';
+import {Events, IonicPage, LoadingController, NavController} from 'ionic-angular';
 import {UserServiceProvider} from "../../providers/user-service/user-service";
+import {ChatType} from "../../model/ChatType";
+import {ConversationServiceProvider} from "../../providers/conversation-service/conversation-service";
 
 /**
  * Generated class for the ListChatPage page.
@@ -15,31 +17,68 @@ import {UserServiceProvider} from "../../providers/user-service/user-service";
 })
 export class ListChatPage {
   listConversation: any;
+  loading: any;
 
-  constructor(private userService: UserServiceProvider, public navCtrl: NavController, public events: Events) {
+  constructor(private userService: UserServiceProvider, public navCtrl: NavController, public events: Events,
+              public loadingCtrl: LoadingController, public conversationService: ConversationServiceProvider) {
     // console.log("ListChatPage constructor");
     this.listConversation = [];
+    this.presentLoadingText("Loading conversation...");
     userService.getUserConversation().subscribe(success => {
         // console.log("ListChatPage getUserConversation success");
         // console.log(success);
-      // TODO : boucler pr add le chattype STD
-        this.listConversation = success;
+      success.forEach((element) => {
+        let newConversation = {
+          name: element.conversation.name,
+          id: element.conversation.id,
+          members: element.conversation.members,
+          last_message: element.conversation.last_message,
+          chatType: ChatType.STD_CONVERSATION
+        };
+        this.listConversation.push(newConversation);
+      });
+        this.loading.dismiss();
       },
       error => {
+        this.loading.dismiss();
         // console.log("ListChatPage getUserConversation error");
         // console.log(error);
       });
 
-    events.subscribe('zone:push', (conversation) => {
-      // TODO : add le type ROOM_CONVER
-      this.listConversation.push(conversation);
+    events.subscribe('zone:push', (conversationId) => {
+      this.conversationService.getConversationById(conversationId).subscribe((success:any) => {
+        let newConversation = {
+          name: success.name,
+          id: success.id,
+          members: success.members,
+          last_message: success.last_message,
+          chatType: ChatType.ROOM_CONVERSATION
+        };
+        this.listConversation.push(newConversation);
+      }, error => {
+        console.log("ListChatPage constructor getConversationById");
+        console.log(error);
+      });
     });
 
     events.subscribe('zone:pop', (conversationId) => {
-      //this.listConversation.push(conversation);
+      this.listConversation = this.remove(this.listConversation, conversationId);
     });
     // // TODO : a virer une fois quon a les init
     //this.listConversation = [{name: "David le poulet", members: [], id: ""}, {name: "La bande a bono", members: [], id: ""}];
+  }
+
+  remove(arrOriginal, conversationId: string){
+    return arrOriginal.filter(function(el){return el.id !== conversationId});
+  }
+
+  presentLoadingText(message: string) {
+    this.loading = this.loadingCtrl.create({
+      spinner: 'hide',
+      content: message
+    });
+
+    this.loading.present();
   }
 
   lunchChatPage(id: string, chatType) {
